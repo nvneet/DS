00f940221acaeaef2dce3944531c3aa8d601c363
nvneet <navneet.ism@gmail.com>
2022-01-23 23:57:10 +0530

    initial commit

---
+++ Recursion/src/recursion/general/DecimalToBinary.java
@@ -0,0 +1,39 @@
+package recursion.general;
+
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+		
+		System.out.println("result: " + decimalToBinary(-13));
+	}
+	
+	private static int decimalToBinary(int num) {
+		
+//		if (num < 0) {
+//			return -1;
+//		}
+		if (num == 0) {
+			return 0;
+		}
+		
+		return num%2 + 10 * decimalToBinary(num / 2);
+		
+	}
+	
+	/*
+	 * step 1: Divide the number by 2
+	 * step 1: Get the integer quotient for the next iteration
+	 * step 3: Get the remainder for the binary digit
+	 * step 4: Repeat the steps until the quotient is equal to 0
+	 * 
+	 * 
+	 * eg. Division by 2         Quotient        Remainder        f(n) = n%2 + 10*f(n/2)
+	 *        13/2                   6               1            110*10 + 1 = 1101
+	 *         6/2                   3               0             11*10 + 0 = 110
+	 *         3/2                   1               1              1*10 + 1 = 11
+	 *         1/2                   0               1            
+	 *         
+	 *         binary for 13: 1101
+	 *         
+	 */
+}
